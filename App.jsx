bed83c09c0d6f19e9867f34da0bd87214a5b66f6
InSange <ehfrur888@naver.com>
2025-04-08 16:21:03 +0900

    Project Init Setting

---
+++ App.jsx
@@ -0,0 +1,13 @@
+// App.jsx
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello, world!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
