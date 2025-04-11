c65efb8b21826a426a88cff8e95b85e677122c5f
aong13 <oaoa13@pukyong.ac.kr>
2025-04-11 13:41:21 +0900

    fix: 테스트용 app.jsx 복원

--- App.jsx
+++ App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 import SearchPage from './src/pages/SearchPage';
-import DeatilPage from './src/pages/DeatilPage';
 
 const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
-      {/* <SearchPage /> */}
-      <DeatilPage />
+      <SearchPage />
     </SafeAreaView>
   );
 };
